9ebd1838c87a0713d1b9a9101aa446f2cc3f9dc6
Himan5hu_5harma <88222961+Ayaan5harma@users.noreply.github.com>
2022-07-26 01:38:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,130 @@
+/* $("#masterplay").click(function(){
+     
+
+    let song = new Audio("Kitni Haseen Hogi.mp3");
+   
+
+    if(song.paused || song.currentTime<=0)
+    {
+        song.play();
+    $("#masterplay").removeClass("fa-circle-play");
+    $("#masterplay").addClass("fa-circle-pause");
+    }
+    else{
+        song.pause();
+        $("#masterplay").removeClass("fa-circle-pause");
+        $("#masterplay").addClass("fa-circle-play");
+    }
+}); */
+let songindex=0;
+let mysong = new Audio("songs/1.mp3");
+let masterplay = document.getElementById("masterplay");
+let myseekbar = document.getElementById("myprogressbar");
+let mygif =document.getElementById("gif");
+let allsong = Array.from(document.getElementsByClassName('songitem'));
+let mastersongname = document.getElementById("mastersongname");
+let songslist =[
+    {songname:"kesariya",filepath:"songs/1.mp3",coverpath:"images/1.jpg"},
+    {songname:"kitni Haseen Hogi",filepath:"songs/2.mp3",coverpath:"images/2.jpg"},
+    {songname:"Rang Lageya",filepath:"songs/3.mp3",coverpath:"images/3.jpg"},
+    {songname:"phir Na Esi raat aayegi",filepath:"songs/4.mp3",coverpath:"images/4.jpg"},
+]
+
+allsong.forEach((element,i)=>{
+    
+    element.getElementsByTagName("img")[0].src =songslist[i].coverpath;
+    element.getElementsByClassName("songname")[0].innerText = songslist[i].songname
+
+});
+
+masterplay.addEventListener("click",()=>{
+    if(mysong.paused || mysong.currentTime<=0)
+    {
+        mysong.play();
+        masterplay.classList.remove("fa-circle-play");
+        masterplay.classList.add("fa-circle-pause");
+        mygif.style.opacity=1;
+    }
+    else{
+        mysong.pause();
+        masterplay.classList.remove("fa-circle-pause");
+        masterplay.classList.add("fa-circle-play");
+        mygif.style.opacity=0;
+    }
+})
+mysong.addEventListener("timeupdate",()=>{
+   // console.log("timeupdate");
+     progress = parseInt((mysong.currentTime/mysong.duration)*100);
+   // console.log(progress);
+    myseekbar.value =progress;
+})
+
+myseekbar.addEventListener("change",()=>{
+    mysong.currentTime = (myseekbar.value*mysong.duration)/100;
+})
+
+const makeallplays =()=>{
+   
+   
+    Array.from(document.getElementsByClassName('songitemplay')).forEach((element)=>{
+        element.classList.remove('fa-circle-pause');
+        element.classList.add('fa-circle-play');
+    })
+}
+
+
+Array.from(document.getElementsByClassName("songitemplay")).forEach((element)=>{
+     
+    element.addEventListener('click',(e)=>{
+        makeallplays();
+        songindex = parseInt(e.target.id);
+        e.target.classList.remove("fa-circle-play");
+        e.target.classList.add("fa-circle-pause");
+        mysong.src='songs/'+songindex+'.mp3'
+        mastersongname.innerText = songslist[songindex-1].songname;
+        mysong.currentTime=0;
+        mysong.play();
+        mygif.style.opacity=1;
+        masterplay.classList.remove('fa-circle-play');
+        masterplay.classList.add('fa-circle-pause');
+
+    })
+})
+
+document.getElementById('next').addEventListener('click', ()=>{
+
+    if(songindex>3)
+    {
+        songindex=1;
+    }
+    else{
+        songindex+=1;
+    }
+    mysong.src='songs/'+songindex+'.mp3'
+    
+    mysong.currentTime=0;
+    mastersongname.innerText = songslist[songindex-1].songname;
+    mysong.play();
+    mygif.style.opacity=1;
+    
+    masterplay.classList.remove('fa-circle-play');
+    masterplay.classList.add('fa-circle-pause');
+   
+});
+document.getElementById('previous').addEventListener('click', ()=>{
+
+    if(songindex<2)
+    {
+        songindex=4;
+    }
+    else{
+        songindex-=1;
+    }
+    mysong.src='songs/'+songindex+'.mp3'
+    mysong.currentTime=0;
+    mastersongname.innerText = songslist[songindex-1].songname;
+    mysong.play();
+    mygif.style.opacity=1;
+    masterplay.classList.remove('fa-circle-play');
+    masterplay.classList.add('fa-circle-pause');
+})
